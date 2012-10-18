8b831663740605737f2dec899cac0fdf0c7d4f3e
Alex Hsieh <amhsieh@gmail.com>
2012-10-18 17:40:53 -0500

    Initial Commit

---
+++ memory_game/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  memory_game
+//
+//  Created by Alex Hsieh on 10/18/12.
+//  Copyright (c) 2012 Alex Hsieh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ memory_game/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  memory_game
+//
+//  Created by Alex Hsieh on 10/18/12.
+//  Copyright (c) 2012 Alex Hsieh. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
